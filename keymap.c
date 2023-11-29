862151de406acfc7570a1de1d93c604e1ec8c1d1
Filipe Chagas <filipe@ochagas.com>
2023-11-29 16:18:59 -0300

    swap num and nav layers

--- keymap.c
+++ keymap.c
@@ -41,13 +41,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //        ├───────────────────┼─────────────────┼─────────────────┼─────────────────┼─────────────────┼───────────────────┤   ├───────────────────┼─────────────────┼─────────────────┼─────────────────┼─────────────────┼──────┤
 //        │       rsft        │        z        │        x        │        c        │        v        │         b         │   │         n         │        m        │        ,        │        .        │        /        │ rsft │
 //        └───────────────────┴─────────────────┴─────────────────┼─────────────────┼─────────────────┼───────────────────┤   ├───────────────────┼─────────────────┼─────────────────┼─────────────────┴─────────────────┴──────┘
-//                                                                │      MO(4)      │      MO(1)      │ MT(MOD_LGUI, spc) │   │ MT(MOD_RGUI, ent) │      MO(2)      │      MO(3)      │                                           
+//                                                                │      MO(1)      │      MO(4)      │ MT(MOD_LGUI, spc) │   │ MT(MOD_RGUI, ent) │      MO(2)      │      MO(3)      │                                           
 //                                                                └─────────────────┴─────────────────┴───────────────────┘   └───────────────────┴─────────────────┴─────────────────┘                                           
 [0] = LAYOUT_split_3x6_3(
       KC_TAB               , KC_Q               , KC_W               , KC_E               , KC_R               , KC_T                 ,     KC_Y                 , KC_U               , KC_I               , KC_O               , KC_P                  , KC_BSPC,
       MT(MOD_LCTL, KC_ESC) , MT(MOD_LCTL, KC_A) , MT(MOD_LALT, KC_S) , MT(MOD_LGUI, KC_D) , MT(MOD_LSFT, KC_F) , KC_G                 ,     KC_H                 , MT(MOD_RSFT, KC_J) , MT(MOD_RGUI, KC_K) , MT(MOD_LALT, KC_L) , MT(MOD_RCTL, KC_SCLN) , KC_QUOT,
       KC_RSFT              , KC_Z               , KC_X               , KC_C               , KC_V               , KC_B                 ,     KC_N                 , KC_M               , KC_COMM            , KC_DOT             , KC_SLSH               , KC_RSFT,
-                                                                       MO(4)              , MO(1)              , MT(MOD_LGUI, KC_SPC) ,     MT(MOD_RGUI, KC_ENT) , MO(2)              , MO(3)                                                                    
+                                                                       MO(1)              , MO(4)              , MT(MOD_LGUI, KC_SPC) ,     MT(MOD_RGUI, KC_ENT) , MO(2)              , MO(3)                                                                    
 ),
 
 //        ┌──────┬──────┬──────┬──────┬──────┬────┐   ┌───────────────┬─────────┬─────────┬───────────┬─────────┬──────┐
